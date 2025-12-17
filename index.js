66821fc46b3925d5a1885bff056cc3aaac9956b9
Selim Mohamed <selimwaleed2@icloud.com>
2025-12-17 14:22:01 +0200

    refactor: update color fetching logic and improve HTML structure for color scheme display

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const form = document.getElementById("color-form");
+const section = document.getElementById("color-scheme");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+  const seedColor = document.getElementById("seed-color").value.slice(1);
+  const modE = document.getElementById("scheme-mode").value;
 
-  fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=5")
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${modE}&count=5`
+  )
     .then((response) => response.json())
     .then((data) => {
-      const colors = data.colors;
       let html = "";
+      const colors = data.colors;
       for (let color of colors) {
-        html += `${color.hex.value}`;
+        html += `<div class="color-column">
+            <div class="color-box" style="background:${color.hex.value}"></div>
+            <p class="hex-value">${color.hex.value}</p>
+            </div>`;
       }
+      section.innerHTML = html;
     });
-  outputContainer.innerHTML = html;
 });
