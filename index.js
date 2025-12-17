a469e147470ca75a46d471bc4e527a5f5910db2d
Selim Mohamed <selimwaleed2@icloud.com>
2025-12-17 14:01:55 +0200

    refactor: implement form submission to fetch and display color scheme

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-fetch("https://www.thecolorapi.com/scheme?hex==FF0&mode=monochrome&count=5")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const form = document.getElementById("color-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  fetch("https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=5")
+    .then((response) => response.json())
+    .then((data) => {
+      const colors = data.colors;
+      let html = "";
+      for (let color of colors) {
+        html += `${color.hex.value}`;
+      }
+    });
+  outputContainer.innerHTML = html;
+});
